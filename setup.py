6ac3665108c410771616a4384d40f4ba9653a5a9
Jay Zeng <jayzeng@jay-zeng.com>
2014-06-23 10:28:25 -0700

    * update url

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
   description = 'Unofficial WalkScore API binding',
   author = 'Jay Zeng',
   author_email = 'jayzeng@jay-zeng.com',
-  url = 'https://github.com/jayzeng/walkscore',
-  download_url = 'https://github.com/jayzeng/walkscore/releases/tag/0.1',
+  url = 'https://github.com/zipdigs/walkscore',
+  download_url = 'https://github.com/zipdigs/walkscore/releases/tag/0.1',
   keywords = ['walkscore', 'api', 'binding'],
   classifiers = [],
 )
