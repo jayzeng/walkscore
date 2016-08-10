dfb6f3bfb9b2ca163eabf8e6c78eab1a6942d62e
Julien Marrec <julien.marrec@gmail.com>
2016-08-10 12:34:25 +0200

    Enhancing test suite to test the walkscore and transitscore APIs. Updated __init__.py (wasn't working). All test and build works fine.

--- test/test.py
+++ test/test.py
@@ -1,12 +1,47 @@
-from api.walkscore import *
+from walkscore.api import *
 from nose.tools import *
+
+# Enter a valid key here
+apiKey='yourapikey'
+if apiKey=='yourapikey':
+   raise InvalidApiKeyException("Please change the apiKey in `test/test.py` before running `python setup.py test`") 
+
 
 @raises(InvalidApiKeyException)
 def test_invalidapi():
-    apiKey='badkey'
-    walkscore = WalkScore(apiKey)
+    badapiKey='badkey'
+    walkscore = WalkScore(badapiKey)
 
     address='somewhere'
     lat = '47.6085'
-    long= '-122.3295'
-    walkscore.makeRequest(address, lat, long)
+    lng= '-122.3295'
+    walkscore.makeRequest(address, lat, lng)
+    
+def test_valid_walskcore_call():
+    
+    walkscore = WalkScore(apiKey)
+
+    address='1119 8th Avenue Seattle WA 98101'
+    lat=47.6085
+    lon=-122.3295
+    jsonResp = walkscore.makeRequest(address, lat, lon)
+    assert(jsonResp['status'] == 1)
+    print(jsonResp)
+    assert(jsonResp['walkscore'] == 97)
+
+    
+def test_valid_transitscore_call():
+
+    transitscore = TransitScore(apiKey)
+
+    # reference https://www.walkscore.com/professional/public-transit-api.php
+    params = {
+        'lat': '47.6101359',
+        'lon': '-122.3420567',
+        'city': 'Seattle',
+        'state': 'WA',
+    }
+
+    jsonResp = transitscore.makeRequest('score', params)
+    print(jsonResp)
+    assert(jsonResp['transit_score'] == 100)
